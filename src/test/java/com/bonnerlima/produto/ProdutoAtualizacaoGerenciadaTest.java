5c7068ef25b629a365347248367d74921eb3d19f
Bonner <bonner@pop-os.localdomain>
2023-09-21 15:52:46 -0300

    add: teste de atualizacao gerenciada do produto

---
+++ src/test/java/com/bonnerlima/produto/ProdutoAtualizacaoGerenciadaTest.java
@@ -0,0 +1,26 @@
+package com.bonnerlima.produto;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.bonnerlima.ManagerTest;
+import com.bonnerlima.model.Produto;
+
+public class ProdutoAtualizacaoGerenciadaTest extends ManagerTest {
+
+    @Test
+    public void insertProdutoTransaction() {
+        Produto p = entityManager.find(Produto.class, 1);
+
+        entityManager.getTransaction().begin();
+        p.setNome("Atualizacao Gerenciada - Kindle");
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Produto produtoAtualizado = entityManager.find(Produto.class, 1);
+
+        Assert.assertEquals("Atualizacao Gerenciada - Kindle", produtoAtualizado.getNome());
+
+    }
+}
