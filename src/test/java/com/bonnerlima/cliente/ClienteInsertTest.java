537dc034b2adfd3ca57491afbd78456e0bf9c9cd
Bonner Lima <neilsonlima@gmail.com>
2023-09-24 16:26:40 -0300

    add: teste insert cliente

---
+++ src/test/java/com/bonnerlima/cliente/ClienteInsertTest.java
@@ -0,0 +1,30 @@
+package com.bonnerlima.cliente;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.bonnerlima.ManagerTest;
+import com.bonnerlima.model.Cliente;
+import com.bonnerlima.model.enuns.Sexo;
+
+public class ClienteInsertTest extends ManagerTest {
+
+    @Test
+    public void insertClienteTransaction() {
+        Cliente cliente = new Cliente();
+        cliente.setId(3);
+        cliente.setNome("Amigo C");
+        cliente.setSexo(Sexo.MASCULINO);
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(cliente);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Cliente clientedicionado = entityManager.find(Cliente.class, cliente.getId());
+
+        Assert.assertNotNull(clientedicionado);
+
+    }
+}
